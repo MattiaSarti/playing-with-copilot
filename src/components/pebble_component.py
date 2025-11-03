44c02d1241a1e9bd37cc8392b1d454477ba414c1
MattiaSarti <nmattia1@gmail.com>
2025-11-03 11:35:05 +0100

    add: source on main

---
+++ src/components/pebble_component.py
@@ -0,0 +1,31 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+import logging
+
+from charmed_kubeflow_chisme.components.pebble_component import PebbleServiceComponent
+from ops.pebble import Layer
+
+logger = logging.getLogger(__name__)
+
+
+class PvcViewerPebbleService(PebbleServiceComponent):
+    def get_layer(self) -> Layer:
+        """Defines and returns Pebble layer configuration
+
+        This method is required for subclassing PebbleServiceContainer
+        """
+        logger.info("PebbleServiceComponent.get_layer executing")
+        return Layer(
+            {
+                "summary": "pvcviewer layer",
+                "description": "Pebble config layer for pvcviewer",
+                "services": {
+                    self.service_name: {
+                        "override": "replace",
+                        "summary": "Entry point for pvcviewer image",
+                        "command": "/manager --health-probe-bind-address=:8081 --metrics-bind-address=:8080 --leader-elect",  # noqa: E501
+                        "startup": "enabled",
+                    }
+                },
+            }
+        )
